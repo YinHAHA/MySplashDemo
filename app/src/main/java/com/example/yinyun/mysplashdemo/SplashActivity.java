999257497178cdbc096ef7edc2b29c1a146e8e8a
yinyun <yinyun@yxqiche.com>
2018-09-21 11:33:41 +0800

    adjustment

--- app/src/main/java/com/example/yinyun/mysplashdemo/SplashActivity.java
+++ app/src/main/java/com/example/yinyun/mysplashdemo/SplashActivity.java
@@ -1,13 +1,13 @@
 package com.example.yinyun.mysplashdemo;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
